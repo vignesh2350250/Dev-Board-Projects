7d831a9c26546ee218194a1e0aa73c844c11cde9
Vignesh <140400414+vignesh2350250@users.noreply.github.com>
2025-06-11 14:07:00 +0530

    Create constraint.v

---
+++ EDGE_Artix_7_FPGA/Full_Adder/constraint.v
@@ -0,0 +1,6 @@
+set_property -dict { PACKAGE_PIN L4    IOSTANDARD LVCMOS33 } [get_ports { A }];
+set_property -dict { PACKAGE_PIN M4    IOSTANDARD LVCMOS33 } [get_ports { B }];
+set_property -dict { PACKAGE_PIN M2    IOSTANDARD LVCMOS33 } [get_ports { Cin  }];
+
+set_property -dict { PACKAGE_PIN H3    IOSTANDARD LVCMOS33 } [get_ports { So }];
+set_property -dict { PACKAGE_PIN J1    IOSTANDARD LVCMOS33 } [get_ports { Co }];
